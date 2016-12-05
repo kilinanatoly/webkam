fd3b53321d2d5c84adb34b60c8cd8e000375443b
kilinanatoly <kilinanatoly@gmail.com>
2016-12-05 18:25:35 +0400

    05.12.2016 18:25

--- controllers/MyController.php
+++ controllers/MyController.php
@@ -2,13 +2,11 @@
 
 namespace app\controllers;
 
+use Madcoda;
 use Yii;
 use yii\filters\AccessControl;
-use yii\web\Controller;
 use yii\filters\VerbFilter;
-use app\models\LoginForm;
-use app\models\ContactForm;
-use Madcoda;
+use yii\web\Controller;
 
 class MyController extends Controller
 {
@@ -65,19 +63,27 @@ class MyController extends Controller
         $youtube = new Madcoda\Youtube(array('key' => 'AIzaSyCJDDnFyY_4DZKXUXKtYbvjmGlB0w_q6uo'));
         $videos1 = NULL;
         $videos2 = NULL;
-        if (Yii::$app->request->post('youtubeSearch')){
-            $videos1 = $youtube->searchVideos(Yii::$app->request->post('youtubeSearch'), 20,'date');
+        if (Yii::$app->request->post('youtubeSearch')) {
+            $videos1 = $youtube->searchVideos(Yii::$app->request->post('youtubeSearch'), 20, 'date');
+            foreach ($videos1 as $key => $value) {
+                $mas[] = $value->id->videoId;
+            }
+            $videosInfo = $youtube->getVideosInfo($mas);
+            usort($videosInfo, function ($a, $b) {
+                return ($a->statistics->viewCount - $b->statistics->viewCount);
+            });
+            $videos2 = array_reverse($videosInfo);
+
         }
-        if (Yii::$app->request->post('youtubeSearch2')){
-            $videos2 = $youtube->searchVideos(Yii::$app->request->post('youtubeSearch2'), 20,'viewcount');
-        }
-        return $this->render('index',[
-            'videos1'=>$videos1,
-            'videos2'=>$videos2,
+        return $this->render('index', [
+            'videos1' => $videos1,
+            'videos2' => $videos2,
         ]);
     }
-    public function actionYoutubeAjax(){
-        if (Yii::$app->request->post('videoId')){
+
+    public function actionYoutubeAjax()
+    {
+        if (Yii::$app->request->post('videoId')) {
             $youtube = new Madcoda\Youtube(array('key' => 'AIzaSyCJDDnFyY_4DZKXUXKtYbvjmGlB0w_q6uo'));
             $video = $youtube->getVideoInfo(Yii::$app->request->post('videoId'));
             $html = $video->player->embedHtml;
--- views/my/index.php
+++ views/my/index.php
@@ -7,7 +7,8 @@ $this->title = 'webkam tz';
 ?>
 <div class="tz-wrap">
     <div class="youtubeForm1">
-        <h2>Задание 1</h2>
+        <h2 class="text-left">Первый экран</h2>
+        
         <?php
         $form = ActiveForm::begin([
             'id' => 'form-input-example',
@@ -29,61 +30,16 @@ $this->title = 'webkam tz';
         ?>
         <div class="clearfix"></div>
     </div>
-    <div class="videos1">
-        <?php
-        if ($videos1) {
-            echo '<div class="videos1__list">';
-            foreach ($videos1 as $key => $value) {
-                echo '<div class="videos1__item">
-                        <div class="videos1__item_img">
-                            <img src="' . $value->snippet->thumbnails->default->url . '" alt="">
-                        </div>
-                        <div class="videos1__item_name">
-                            ' . $value->snippet->title . '
-                        </div>
-                        <div class="videos1__item_date">
-                            Опубликовано : ' . date('d-m-Y',strtotime($value->snippet->publishedAt)) . '
-                        </div>
-                    </div>';
-            }
-            echo '</div>';
-        }else{
-            echo '<p class="youtube_nf">По данному запросу ничего не найдено либо запрос пуст</p>';
-        }
-        ?>
-    </div>
 
-    <div class="youtubeForm2">
-        <h2>Задание 2</h2>
-        <?php
-        $form = ActiveForm::begin([
-            'id' => 'form-input-example',
-            'options' => [
-                'class' => 'form-horizontal col-lg-12',
-                'enctype' => 'multipart/form-data'
-            ],
-        ]);
-        ?>
-        <div class="youtubeInp">
-            <input type="text" class="form-control" value="<?=Yii::$app->request->post('youtubeSearch2')?>"  placeholder="Введите поисковый запрос" name="youtubeSearch2">
-        </div>
-        <div class="youtubeSubm">
-            <input type="submit" class="btn btn-primary" value="Поиск">
-        </div>
-        <?php
-        // Содержимое формы
-        ActiveForm::end();
-        ?>
-        <div class="clearfix"></div>
-
-    </div>
+   <hr>
     <div class="videos2">
+	<h2 class="text-left">Второй экран</h2>
         <?php
         if ($videos2) {
             echo '<div class="videos1__list">';
             foreach ($videos2 as $key => $value) {
                 echo '<div class="videos2__item">
-                        <a href="#" class="videos2__item_href" data-id="'.$value->id->videoId.'" >
+                        <a href="#" class="videos2__item_href" data-id="'.$value->id.'" >
                             <div class="videos2__item_img">
                                 <img src="' . $value->snippet->thumbnails->default->url . '" alt="">
                             </div>
@@ -98,6 +54,9 @@ $this->title = 'webkam tz';
                      
                             <div class="videos2__item_athor">
                                 Автор : ' . $value->snippet->channelTitle . '
+                            </div>
+                            <div class="videos2__item_athor">
+                                Просмотров : ' . $value->statistics->viewCount . '
                             </div>
                         
                         
